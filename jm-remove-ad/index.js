452dc027b648a1dbb40c1caf20d04972d90e46e5
Mesimpler <qijieh1999@gmail.com>
2025-06-21 12:09:13 +0800

    update

--- jm-remove-ad/index.js
+++ jm-remove-ad/index.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://18comic.vip/*
 // @grant       GM_addStyle
-// @version     1.0.1
+// @version     2.0.0
 // @author      mesimpler
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description JM漫画去广告
@@ -20,28 +20,57 @@ function applyToAllElements(selector, callback) {
      elements.forEach(callback)
   }
 }
-
-applyToElement('.div-bf-pv', (el) => el.remove())
-applyToElement('.top-nav', (el) => el.remove())
-applyToElement('#Comic_Top_Nav', (el) => el.style.top = 0)
-applyToElement('img[src*="float-right-close.png"]', (el) => el.parentElement.remove())
-applyToElement('div[class*="sticky"]', (el) => el.remove())
-
-applyToAllElements('.meun-game', (el) => el.parentElement.remove())
-applyToAllElements('.meun-video', (el) => el.parentElement.remove())
+function removeElements(selectors) {
+  selectors.forEach((selector) => applyToAllElements(selector, (el) => el.remove()));
+}
 
 GM_addStyle(`
+  #Comic_Top_Nav {
+    top: 0 !important;
+    margin-bottom: 0 !important;
+  }
+  [style*="position: fixed"][style*="overflow: hidden"][style*="margin: 0px auto"][style*="transform: translate3d(0px, 0px, 0px) translateX(-50%)"][style*="height: auto"][style*="box-sizing: border-box"][style*="display: flex"][style*="bottom: 0px"][style*="left: 50%"] {
+    display: none !important;
+  }
+  /* 底部Tab补齐 */
   .ph-bottom li {
     width: 100% !important;
   }
 `)
 
-setInterval(() => {
-  applyToAllElements('div[data-show="ok"]', (el) => el.remove())
-}, 3000)
+const ADS_SELECTORS = [
+  `[data-group="photo_center_games_1"]`, //最上面的广告
+  `[data-group="photo_center_1"]`, //中间的广告
+  `[data-group="photo_bottom1"]`, //最底下的广告
+  `[data-group="photo_bottom2"]`, //最底下的广告
+  `[data-group="photo_bottom3"]`, //最底下的广告
+  `[data-group="photo_bottom4"]`, //最底下的广告
+  `[style*="text-align:center"][style*="margin: 0 auto"][style*="max-width: 100%"]`, //播放完的广告
+  ".float_right", //靠右的三个操作按钮
+  ".center.scramble-page.thewayhome", //回家的路
+  `[data-group="album_detail"]`, // 本子页Tag部分广告贴片
+  `[data-group="web_comicforum_middle1"]`, // 评论区底部追加广告
+  ".ph-active:has(.meun-video)", // 小电影Tab
+  ".ph-active:has(.meun-game)", // 游戏Tab
+  ".mobile-ad",
+  `div[class$="b_sticky2"]`,
+  `[data-group="album_related1"]`,
+  `[data-group="album_related2"]`,
+  `[data-group="album_related3"]`,
+  `[data-group="album_related4"]`,
+  `[data-group="all_bottom1"]`,
+  `[data-group="all_bottom2"]`,
+  `[data-group="all_bottom3"]`,
+  `[data-group="all_bottom4"]`,
+  `[data-group="all_bottom5"]`,
+  `[data-group="all_bottom6"]`,
+  `[data-group="all_bottom7"]`,
+  `[data-group="all_bottom8"]`,
+  ".top-nav",
+  ".div-bf-pv",
+];
 
-// const targetNode = document.querySelector("#wrapper");
-// const disconnect = VM.observe(targetNode, () => {
-//   applyToAllElements('div[data-show="ok"]', (el) => el.remove())
-// });
+VM.observe(document.body, () => {
+  removeElements(ADS_SELECTORS)
+});
 
---
+++ jm-remove-ad/readme.md
@@ -0,0 +1,7 @@
+# jm-remove-ad
+
+禁漫去广告
+
+[![2025-06-21-120452.png](https://i.postimg.cc/rFYZJspv/2025-06-21-120452.png)](https://postimg.cc/sv7JfV7c)
+
+在[uncharity](https://sleazyfork.org/zh-CN/users/1464366)的相关脚本[禁漫去广告](https://sleazyfork.org/zh-CN/scripts/534625)上追加部分未屏蔽的广告，并移除底部电影和游戏的页签。
