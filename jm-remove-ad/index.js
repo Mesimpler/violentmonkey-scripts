dab80f1a5d821b35c99025cb1d66b615809c3937
Mesimpler <qijieh1999@gmail.com>
2025-07-10 17:24:48 +0800

    updata

--- jm-remove-ad/index.js
+++ jm-remove-ad/index.js
@@ -5,69 +5,95 @@
 // @match       https://18comic.vip/*
 // @run-at      document-start
 // @grant       GM_addStyle
-// @version     2.1.7
+// @version     3.0.0
 // @author      mesimpler
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @description JM漫画去广告
 // ==/UserScript==
-function applyToElement(selector, callback) {
-    const element = document.querySelector(selector)
-    if (element) callback(element);
-  }
-  function applyToAllElements(selector, callback) {
-    const elements = document.querySelectorAll(selector);
-    if(elements) {
-      elements.forEach(callback)
+function removeElements(selectors) {
+  selectors.forEach((selector) => {
+    const elements = document.querySelectorAll(selector)
+    if (elements) {
+      elements.forEach(el => el.remove())
     }
-  }
-  function removeElements(selectors) {
-    selectors.forEach((selector) => applyToAllElements(selector, (el) => el.remove()));
+  });
+}
+
+GM_addStyle(`
+  /* 修复去除顶部外链后的空白间隔 */
+  #Comic_Top_Nav {
+    top: 0 !important; 
   }
 
-  GM_addStyle(`
-    #Comic_Top_Nav {
-      top: 0 !important;
-      margin-bottom: 0 !important;
-    }
-    [style*="position: fixed"][style*="overflow: hidden"][style*="margin: 0px auto"][style*="transform: translate3d(0px, 0px, 0px) translateX(-50%)"][style*="height: auto"][style*="box-sizing: border-box"][style*="display: flex"][style*="bottom: 0px"][style*="left: 50%"] {
-      display: none !important;
-    }
-    /* 底部Tab补齐 */
-    .ph-bottom li {
-      width: 100% !important;
-    }
-  `)
+  /* 修复标题栏没对齐的问题(JM前端代码写得和狗屎一样) */
+  #Comic_Top_Nav>.container>.navbar-header {
+    display: flex;
+    align-items: center !important;
+    justify-content: space-between;
+  }
+  .navbar-toggle {
+    margin-right: 0 !important;
+  }
+  a[href="#shunt-modal"] {
+    margin-top: 0 !important;
+  }
+  .head-right.visible-xs.visible-sm {
+    padding: 0 !important;
+  }
 
-  const ADS_SELECTORS = [
-    `[data-group="photo_center_games_1"]`, //最上面的广告
-    `[data-group="photo_center_1"]`, //中间的广告
-    `[data-group*="photo_bottom"]`, //最底下的广告
-    `[style*="text-align:center"][style*="margin: 0 auto"][style*="max-width: 100%"]`, //播放完的广告
-    ".center.scramble-page.thewayhome", //回家的路
-    `[data-group="album_detail"]`, // 本子页Tag部分广告贴片
-    `[data-group="web_comicforum_middle1"]`, // 评论区底部追加广告
-    `.float_right>div:has(a[href="#app_modal"])`, // app下载按钮
-    ".ph-active:has(.meun-video)", // 小电影Tab
-    ".ph-active:has(.meun-game)", // 游戏Tab
-    `.ph-active:has(a[href="/veteran"])`, // 好站推荐Tab
-    ".mobile-ad",
-    ".header-menu-block", // 个人页 标题栏
-    ".user-daily-mission", // 个人页日常任务
-    `div[class$="b_sticky2"]`,
-    `[data-group*="album_related"]`,
-    `[data-group*="all_bottom"]`,
-    `.row:has(div[data-group*="all_albums"])`, // 搜索页底部广告
-    `.row:has(div[data-group*="list"])`, // 类别页广告
-  ];
+  /* 修复去除首页滚动广告后grid菜单与顶部的空白间隔问题 */
+  body {
+    padding-top: 70px !important;
+  }
 
-  removeElements(ADS_SELECTORS)
-  
-  if (document.body) {
+  /* 返回顶部悬浮按钮 
+  .float_right {
+    top: 80% !important;
+  }*/
+
+  /* 底部Tab补齐 */
+  .ph-bottom li {
+    width: 100% !important;
+  }
+`)
+
+const ADS_SELECTORS = [
+  `.modal-dialog.billboard-modal`, //>18警告模态框
+  `#billboard-modal.modal.fade.in`, `.modal-backdrop.fade.in`, // >18警告模态框背景遮罩
+  `.top-nav:has(.top-bar)`, // 顶部广告外链
+  `.div-bf-pv`, // 首页滚动广告
+  `.float_right > div:has(p a[rel="nofollow noopener"])`, // 与悬浮返回顶部按钮一起的广告下载按钮
+  `.float_right > div:has(a[href="#app_modal"])`, // 与悬浮返回顶部按钮一起的app下载按钮
+  `div[data-group*="sticky"]`, // 首页的右下角弹窗广告
+  `[data-group*="all_bottom"]`, // 插入在信息流中的广告画廊
+  `.ph-active:has(.meun-video)`, // 小电影Tab
+  `.ph-active:has(.meun-game)`, // 游戏Tab
+  `.ph-active:has(a[href="/veteran"])`, // 好站推荐Tab
+
+  `.m-header-info-profile`, // 个人资料页 繁杂信息
+  `.user-daily-mission`, // 个人资料页 个人任务
+
+  `div[style="display: flex;margin-bottom:10px;justify-content: center;"]`, // 书库页 顶部广告贴片
+  `.row:has(div[data-group*="list"])`, // 类别页 插入的贴片广告
+  `div[data-group*="board"]`, // 评论页 头部广告
+  `.row:has(div[data-group*="all_albums"])`, // 搜索页 底部广告
+
+  `.row:has(div[class*="mobile-ad"])`, // 本子详情页 顶部广告
+  `div[data-group="album_detail"]`, // 本子详情页 分集处插入广告
+  `.row:has(div[data-group*="album_related"])`, // 本子详情页 底部广告
+
+  `[data-group*="photo_center"]`, // 本子阅读页 顶部广告
+  `.row:has(div[data-group*="photo_bottom"])` // 本子阅读页 底部广告
+];
+
+removeElements(ADS_SELECTORS)
+
+if (document.body) {
+  VM.observe(document.body, () => removeElements(ADS_SELECTORS));
+} else {
+  document.addEventListener("DOMContentLoaded", () => {
     VM.observe(document.body, () => removeElements(ADS_SELECTORS));
-  } else {
-    document.addEventListener("DOMContentLoaded", () => {
-      VM.observe(document.body, () => removeElements(ADS_SELECTORS));
-    });
-  }
+  });
+}
 
-  window.addEventListener("load", removeElements(ADS_SELECTORS));
+window.addEventListener("load", removeElements(ADS_SELECTORS));
